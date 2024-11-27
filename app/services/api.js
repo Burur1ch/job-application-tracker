0b9f276605d12780fb7bbea89f96661a0f2435e4
Ilya <irresistible0@mail.ru>
2024-11-27 13:06:05 +0000

    Fixed

--- app/services/api.js
+++ app/services/api.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 const API_URL = "https://qmvrmx-5000.csb.app/api/applications";
-=======
-const API_URL = "http://localhost:5000/api/applications";
->>>>>>> 29b0bd5c1d8ca4629e34f0ed8b0148cb0f2ac0aa
+
+//const API_URL = "http://localhost:5000/api/applications";
 
 // Получить все записи
 export const fetchApplications = async () => {
@@ -12,15 +10,7 @@ export const fetchApplications = async () => {
 };
 
 // Добавить запись
-<<<<<<< HEAD
 
-// Используйте API_URL везде:
-export const createApplication = async (application) => {
-  //codesandbox.io/p/github/Burur1ch/job-application-tracker/main?workspaceId=2b2048ed-8f67-4ea8-912d-f11f62375be4
-  https: if (!response.ok) {
-    throw new Error("Ошибка при создании записи");
-  }
-=======
 export const createApplication = async (application) => {
   const response = await fetch(`${API_URL}`, {
     method: "POST",
@@ -34,7 +24,6 @@ export const createApplication = async (application) => {
     throw new Error("Ошибка при создании записи");
   }
 
->>>>>>> 29b0bd5c1d8ca4629e34f0ed8b0148cb0f2ac0aa
   return response.json();
 };
 
